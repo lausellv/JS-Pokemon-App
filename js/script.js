4338b95167db27b0b981c24bfd7fa3c151834b67
lausellv <lausellv@yahoo.com>
2020-11-11 20:22:18 +0100

    final changes to 1.6 DOM assignement

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
-document.write('<h3>==== JS 1.6 Part 1 =======</h3>');
-let pokemonList = []; // no need to add anything now
 
+let pokemonList = []; // no need to add anything now
+//https://www.pokemon.com/us/pokedex/
 // Then we can use the Array.push function to add things
 pokemonList.push({
   name: 'Bulbasaur',
@@ -21,102 +21,136 @@ pokemonList.push({
   height: 1.7
 });
 
-let butterfree = {name: 'Butterfree',type: ['bug', 'flying'], height: 1.1}; 
+let butterfree = { name: 'Butterfree', type: ['bug', 'flying'], height: 1.1 };
 
-let pokemon1 = 
-    {name: 'Charmeleon',
-   type: 'fire',
-     height: 1.1
-   };
+let pokemon1 =
+{
+  name: 'Charmeleon',
+  type: 'fire',
+  height: 1.1
+};
 
- let pokemon2 = {name:'Rattata',
-   type: 'normal',
-   height: 0.3};
-
-function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
-  add(butterfree);  // push these object values through the add function
-  add(pokemon1);
-  add(pokemon2);
-  
-  
-
-// pokemonList.forEach(function(keys){  // creating a ul list. Using forEach to create separate buttons for each pokemon.
-
-//   let pokemonIndex = document.querySelector(".pokemon-list");
-//     let listpokemon = document.createElement("li");
-//     let button = document.createElement("button");
-
-//     button.innerText = keys.name;
-//     button.classList.add("btn");
-
-//     listpokemon.append(button);
-//     pokemonIndex.append(listpokemon);
-
-// });
+let pokemon2 = {
+  name: 'Rattata',
+  type: 'normal',
+  height: 0.3
+};
 
 
+let pokemon3= {name:'Squirtle',
+type:'water',
+height:1.08
+};
 
 
 let pokemonRepository = (function () {
- pokemonList;
+  pokemonList;
 
-  function getAll () {
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+  function getAll() {
     return pokemonList;
   }
 
-function addListItem(pokemon){
-let pokemonIndex = document.querySelector(".pokemon-list");
+  function showDetails(pokemon){
+    console.log('Name: ' + pokemon.name +' Type: ' +pokemon.type + ' Height: ' + pokemon.height);
+  }
+  function addListItem(pokemon) {
+    let pokemonIndex = document.querySelector(".pokemon-list");
+    //creating the list item
     let listpokemon = document.createElement("li");
+    //creating the button, adding a class to it
     let button = document.createElement("button");
-
     button.innerText = pokemon.name;
     button.classList.add("btn");
 
+//appending button and list to DOM
     listpokemon.append(button);
     pokemonIndex.append(listpokemon);
-    
+// adding an event listener to button
+button.addEventListener('click', function (event) {
+  let target = event.target;
+  target.classList.toggle('button--green');
+  // shows the pokemon object to the console
+  showDetails(pokemon);
+  
+});
 
-}
- return {
-   
+
+
+  }
+  return {
+    
     add: add,
-    getAll: getAll, 
+    getAll: getAll,
+    // adding the function to the object returned from the IIFE so I will have it outside
     addListItem: addListItem,
   };
 
 }());
 
+// this is how we add new pokemons
+pokemonRepository.add(butterfree);
+pokemonRepository.add(pokemon1);
+pokemonRepository.add(pokemon2);
+pokemonRepository.add(pokemon3);
 
-console.log(pokemonRepository);  // returns the function in the console
-console.log (pokemonRepository.getAll());  // this returns the array in the console
-//document.write(pokemonRepository.getAll());  // this returns [object Object],[object Object],[object Object],[object Object] on the web page
+console.log(pokemonRepository.getAll());  // this returns the array in the console
 
-// I would like to print out a list of the pokemons :-(
-  pokemonRepository.getAll().forEach(function(pokemon){
-     pokemonRepository.addListItem(pokemon);
-    //  let pokemonIndex = document.querySelector(".pokemon-list");
-    // let listpokemon = document.createElement("li");
-    // let button = document.createElement("button");
+//this will 
+pokemonRepository.getAll().forEach(function (pokemon) {
+//   
+//all this code move inside de IIEM
 
-    // button.innerText = pokemon.name;
-    // button.classList.add("btn");
-
-    // listpokemon.append(button);
-    // pokemonIndex.append(listpokemon);
-
-  });
-
-
-
-
+//assigning the ul element to pokemonIndex
+//   let pokemonIndex = document.querySelector(".pokemon-list");
   
+//creating the list item
+//    let listpokemon = document.createElement("li");
+
+//creating the button, adding a class to it and appending the name to innerText
+//    let button = document.createElement("button");
+//    button.innerText = pokemon.name;
+//    button.classList.add("btn");
+
+//  appending the list and the button
+//    listpokemon.append(button);
+//     pokemonIndex.append(listpokemon);
+
+// this is how we call the function addlistItem inside the IIEM
+pokemonRepository.addListItem(pokemon);
 
 
-   
-   
+});
+// // Examining the document Object
+// //console.dir(document);
+// console.log(document.domain);
+// console.log(document.url);
+// console.log(document.title);
+// //document.title = "Pokemon App";
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// //console.log(document.all[13]);
+// //document.all[13].textContent = 'Hello, It\'s me, Victor!';
+// console.log(document.all[8]);
+// console.log(document.links);  // returns HTMLCollection { length: 0 } bc there are no links
+
+// Selecting methods for the DOM
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
